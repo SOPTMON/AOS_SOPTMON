89393a73a8dfa72418c0a895ac7dcb189ac413c4
스프링 피바라기 <daehwan7989@naver.com>
2022-11-22 14:33:04 +0900

    HomeActivity 구성

--- app/src/main/java/com/sopt/soptmon/daehwan/HomeActivity.kt
+++ app/src/main/java/com/sopt/soptmon/daehwan/HomeActivity.kt
@@ -1,12 +1,59 @@
 package com.sopt.soptmon.daehwan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.sopt.soptmon.R
+import com.sopt.soptmon.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_home, HomeFragment())
+            .commit()
+
+        binding.btNavBar.setOnItemSelectedListener {
+            BottomMenu
+                .from(it.title.toString().uppercase())
+                .changeFragment(supportFragmentManager)
+
+            true
+        }
+    }
+
+    enum class BottomMenu() {
+        HOME {
+            override fun changeFragment(supportFragmentManager: FragmentManager) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fcv_home, HomeFragment())
+                    .commit()
+            }
+        },
+        CATEGORY {
+            override fun changeFragment(supportFragmentManager: FragmentManager) {
+                // TODO : to Category fragment
+            }
+        }
+        ;
+
+        abstract fun changeFragment(supportFragmentManager: FragmentManager)
+
+        companion object {
+            fun from(title: String): BottomMenu {
+                return when (title) {
+                    "HOME" -> HOME
+                    else -> {
+                        throw IllegalArgumentException("invalid home fragment name")
+                    }
+                }
+            }
+        }
     }
 }
