0093ded34a9d462f7393aa79d4595dadd9545f4a
스프링 피바라기 <daehwan7989@naver.com>
2022-11-22 13:19:50 +0900

    Home view base 구현

--- app/src/main/java/com/sopt/soptmon/daehwan/HomeActivity.kt
+++ app/src/main/java/com/sopt/soptmon/daehwan/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.sopt.soptmon
+package com.sopt.soptmon.daehwan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopt.soptmon.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
